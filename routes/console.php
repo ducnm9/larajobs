0a7c9bfab8bf982fbb0422ddba83eac46f9baf43
duc_duc <duc@forsharing.info>
2024-06-05 21:53:25 +0700

    c

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,9 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//})->purpose('Display an inspiring quote')->hourly();
 
 
 Schedule::command('app:send-log5minute')->everyFiveMinutes();
